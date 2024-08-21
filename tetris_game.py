8e3cc0e6989ba7362506f6b52cb0b79f6b7b9b50
Sergei <hentai.exe@gmail.com>
2024-08-21 21:22:37 +0300

    next

--- tetris_game.py
+++ tetris_game.py
@@ -9,7 +9,8 @@ running = True
 GRID_ROWS = 25
 GRID_COLS = 15
 SQUARE_SIZE = 20
-SPEED_FACTOR = 0.2
+INITIAL_SPEED_FACTOR = 0.2
+SPEED_FACTOR = INITIAL_SPEED_FACTOR
 
 pygame.init()
 screen = pygame.display.set_mode((GRID_COLS * SQUARE_SIZE,
@@ -26,6 +27,8 @@ player.add_on_grid(grid)
 last_time = time.time()
 last_move_time = 0
 
+accelerate_fall = False
+
 while running:
     clock.tick(60)
     last_move_time = last_move_time + time.time() - last_time
@@ -36,21 +39,32 @@ while running:
         if not FigureMovement.move_down(player, grid):
             player = figure_factory.random(4, 0)
             player.add_on_grid(grid)
+            accelerate_fall = False
+            SPEED_FACTOR = INITIAL_SPEED_FACTOR
 
     screen.fill("White")
     grid.draw(screen)
     pygame.display.update()
 
+    pressed = pygame.key.get_pressed()
+    if pressed[pygame.K_DOWN] and accelerate_fall:
+        SPEED_FACTOR = 0.01
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             running = False
-        if event.type == pygame.KEYDOWN:
+        elif event.type == pygame.KEYDOWN:
             if event.key in [pygame.K_UP, pygame.K_k]:
                 FigureMovement.rotate(player, grid)
             elif event.key in [pygame.K_DOWN, pygame.K_j]:
-                FigureMovement.fall_down(player, grid)
+                accelerate_fall = True
+                # FigureMovement.fall_down(player, grid)
             elif event.key in [pygame.K_RIGHT, pygame.K_l]:
                 FigureMovement.move_figure_right(player, grid)
             elif event.key in [pygame.K_LEFT, pygame.K_h]:
                 FigureMovement.move_figure_left(player, grid)
+        elif event.type == pygame.KEYUP:
+            if event.key in [pygame.K_DOWN, pygame.K_j]:
+                accelerate_fall = False
+                SPEED_FACTOR = INITIAL_SPEED_FACTOR
